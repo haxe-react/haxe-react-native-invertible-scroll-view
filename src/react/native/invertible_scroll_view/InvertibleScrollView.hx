0b9cc1c71cad44128fb5f08094317b4a2f5041b6
terence <terence@awesapp.com>
2017-01-18 16:31:49 +0800

    fix

--- src/react/native/invertible_scroll_view/InvertibleScrollView.hx
+++ src/react/native/invertible_scroll_view/InvertibleScrollView.hx
@@ -1,4 +1,4 @@
 package react.native.invertible_scroll_view;
 
-@:jsRequire('react-native-invertible-scroll-view')
+@:jsRequire('react-native-invertible-scroll-view', 'default')
 extern class InvertibleScrollView extends react.ReactComponent {}
