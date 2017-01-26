c778655e757a8555ed0eb7b2023afd32f64f39f6
Kevin Leung <kevinresol@gmail.com>
2017-01-26 15:59:12 +0800

    Revert "fix"
    This reverts commit 0b9cc1c71cad44128fb5f08094317b4a2f5041b6.

--- src/react/native/invertible_scroll_view/InvertibleScrollView.hx
+++ src/react/native/invertible_scroll_view/InvertibleScrollView.hx
@@ -1,4 +1,4 @@
 package react.native.invertible_scroll_view;
 
-@:jsRequire('react-native-invertible-scroll-view', 'default')
+@:jsRequire('react-native-invertible-scroll-view')
 extern class InvertibleScrollView extends react.ReactComponent {}
